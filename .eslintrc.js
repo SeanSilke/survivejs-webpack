b93c2efd915c7006b234428939596ab9c894a975
Sergey Orlov <OrlovMail@gmail.com>
2017-06-27 21:48:28 +0300

    Add eslint  plugin import

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    `plugin:import/errors`,
+    'plugin:import/warnings'
+  ],
   parserOptions: {
     sourceType: 'module',
   },
